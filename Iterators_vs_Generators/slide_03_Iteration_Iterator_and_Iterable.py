5caa493862cf623c684d3030d71c5268ae99dde0
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-08-02 22:04:50 +0300

    Added slide 03 code

---
+++ Iterators_vs_Generators/slide_03_Iteration_Iterator_and_Iterable.py
@@ -0,0 +1,10 @@
+for a in 'SoftServe':
+   pass
+for b in [19, 15, 6, 20, 19, 5, 18, 22, 5]:
+   pass
+for c in (19, 15, 6, 20, 19, 5, 18, 22, 5):
+   pass
+for d in {19, 15, 6, 20, 19, 5, 18, 22, 5}:
+   pass
+for e in range(9):
+   pass
